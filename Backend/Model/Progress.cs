9019c02fe4875c7a530f287388f3a3dbc4c94067
Sujal-Bangari <167900534+Sujal-Bangari@users.noreply.github.com>
2025-05-29 14:06:28 +0530

    Create Progress.cs

---
+++ Backend/Model/Progress.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace retplann.Model
+{
+    public class Progress
+    {
+        //public int GId {get; set;}
+        public decimal Total_Savings{get; set;}
+
+    }
+}
